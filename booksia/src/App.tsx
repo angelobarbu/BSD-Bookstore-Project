6d1bcded4b2361bb3a3981cdb5e72f152762d0cc
daria <dariae9@yahoo.com>
2025-01-04 22:05:13 +0200

    create wishlist dialog

--- booksia/src/App.tsx
+++ booksia/src/App.tsx
@@ -2,12 +2,15 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "@/pages/HomePage.jsx";
 import NoPage from "@/pages/NoPage.jsx";
+import WishlistPage from "@/pages/WishlistPage.jsx";
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route index element={<HomePage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/wishlist" element={<WishlistPage />} />
                     <Route path="*" element={<NoPage />} />
                 </Routes>
             </BrowserRouter>
