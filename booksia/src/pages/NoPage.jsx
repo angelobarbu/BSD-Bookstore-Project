42524a339763434d2a38b63875096bd30590d1e8
daria <dariae9@yahoo.com>
2025-01-04 21:25:30 +0200

    create basic 404 page

--- booksia/src/pages/NoPage.jsx
+++ booksia/src/pages/NoPage.jsx
@@ -1,7 +1,23 @@
+import {Link} from "react-router-dom";
+import Navbar from "@/components/Navbar.jsx";
+import Footer from "@/components/Footer.jsx";
+
 export default function NoPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-3xl">404 | Page Not Found</h1>
-        </div>
+        <>
+            <Navbar></Navbar>
+            <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+                <h1 className="text-6xl sm:text-8xl lg:text-9xl">404</h1>
+                <p className="text-lg sm:text-xl mt-4">This page got lost between our racks.</p>
+                <Link to="/">
+                    <p className="text-lg sm:text-xl mt-4">
+                        Get back <span className="underline underline-offset-2">home</span>.
+                    </p>
+                </Link>
+            </div>
+
+            <Footer></Footer>
+        </>
+
     )
 };
