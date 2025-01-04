8f84dfc8bc851ee79d9aecd6f1b06450f97f59a9
daria <dariae9@yahoo.com>
2025-01-04 16:10:14 +0200

    create search input component

--- booksia/src/App.tsx
+++ booksia/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Button} from "@/components/ui/button.tsx";
 import Navbar from "@/components/navbar.jsx";
+import SearchInput from "@/components/SearchInput.jsx";
 
 function App() {
   return (
     <>
         <Navbar />
         <Button>Hello</Button>
+        <SearchInput></SearchInput>
     </>
   )
 }
