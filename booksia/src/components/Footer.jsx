6a10bc8d94f383cdb1c73a77b2eeaf76c4cbfa0d
daria <dariae9@yahoo.com>
2025-01-04 22:35:45 +0200

    make the footer responsive

--- booksia/src/components/Footer.jsx
+++ booksia/src/components/Footer.jsx
@@ -1,10 +1,8 @@
-
-
-function Footer () {
+function Footer() {
     return (
-        <footer className="flex text-white bg-black p-2 pb-10">
-            <p className="text-3xl p-3 mr-5">booksia</p>
-            <div className="py-2 mr-4">
+        <footer className="flex flex-col sm:flex-row text-white bg-black p-4 pb-10">
+            <p className="text-3xl p-3 sm:p-0 mr-5">booksia</p>
+            <div className="py-2 mr-5">
                 <p className="text-lg pb-1">About us</p>
                 <p className="text-sm text-gray-400 pb-1">Forgot password</p>
                 <p className="text-sm text-gray-400 pb-1">Shipping and returns</p>
@@ -17,9 +15,9 @@ function Footer () {
                 <p className="text-sm text-gray-400 pb-1">Cookies</p>
                 <p className="text-sm text-gray-400 pb-1">Privacy Policy</p>
             </div>
-
         </footer>
-    )
+    );
 }
+
 
 export default Footer;
